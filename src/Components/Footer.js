6e7a3ba1ab3b37693659cd2cc8c6c4a7a415d51a
abdullah qureshi <hafizabdullahqurashi1@gmail.com>
2023-01-23 13:14:55 +0500

    fifth

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+import Footerdetail from './Footerdetail';
+const Footer = () => {
+    const [state, setstate] = useState(initialState)
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Footer;
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -24,7 +24,7 @@ const Main = () => {
        
         </div>
       </div>
-      hhhhhhhh
+     
     </div>
   )
 }
